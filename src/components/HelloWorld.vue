5d6906d8ab70c1e456c86f85c7990e3c4db816c2
Fábio Gonçalves <contatofabiofg@hotmail.com>
2022-11-26 23:12:04 -0300

    Modelagem inicial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <h1>Fábio Gonçalves</h1>
+    <h2>Frontend Developer - Web Interface and Mobile</h2>
+
+    <h3>Projects and Technologies</h3>
+
+    <section>
+      <h4>SuperChord</h4>
+
+      <p>
+        System developed to assistant the instrumentists's studies/rehershalof;
+        In this it's possible to write chords and, in a simple touch, change the
+        tonality, modify chords, to include basses by drag and drop, to print
+        PDF and export/import in editable file. The chords concerning to the
+        tonality's grade are ordered and displayed initially but, at expand the
+        controll bar, it is possible to add chords that are out the grade.
+        Developed with Vite, Vue.js and TailWind. Another features available:
+        DarkMode and Zoom. Platform: web.
+      </p>
+    </section>
+  </div>
+</template>
