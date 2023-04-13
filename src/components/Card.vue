f1029896ffe34b91690e32e5264034b0f6a094d6
FULLSCREEN\fabio.nascimento <fabio.nascimento@fullscreen.com.br>
2023-04-13 09:11:32 -0300

    Imagens ajustadas para serem exibidas na Vercel

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <div
-    class="duration-200 hover:scale-[102%] w-[90vw] lg:w-[30vw] p-4 m-3 mb-10 bg-white rounded flex flex-col items-center drop-shadow-md border border-slate-200"
+    class="w-[90vw] lg:w-[30vw] p-4 m-3 mb-10 bg-white rounded flex flex-col items-center drop-shadow-md border border-slate-200"
   >
     <div
       class="flex justify-center items-center w-full text-center pb-2 mb-3 text-2xl font-bold border-b border-slate-300"
@@ -32,6 +32,12 @@ defineProps({
           alt="Vue logo"
           class="w-5 mx-2 opacity-50"
         />
+        <img
+          v-if="card.typescript"
+          src="../assets/typescript.png"
+          alt="Typescript logo"
+          class="w-5 mx-2 opacity-50"
+        />
 
         <img
           v-if="card.tailwind"
@@ -54,21 +60,78 @@ defineProps({
       </div>
     </div>
 
-    <img :src="card.logo" alt="Logo" class="w-[80%] mb-3" />
+    <img
+      v-if="card.log == 'superchord'"
+      src="../assets/superchord.png"
+      alt="Logo"
+      class="w-[80%] mb-3"
+    />
+    <img
+      v-if="card.log == 'secretfriend'"
+      src="../assets/secretfriend.png"
+      alt="Logo"
+      class="w-[80%] mb-3"
+    />
+    <img
+      v-if="card.log == 'happybirthday'"
+      src="../assets/happybirthday.png"
+      alt="Logo"
+      class="w-[80%] mb-3"
+    />
+    <img
+      v-if="card.log == 'pokedex'"
+      src="../assets/pokedex.png"
+      alt="Logo"
+      class="w-[80%] mb-3"
+    />
+    <img
+      v-if="card.log == 'ecommerce'"
+      src="../assets/ecommerce.png"
+      alt="Logo"
+      class="w-[80%] mb-3"
+    />
     <p>
       {{ card.description }}
     </p>
 
-    <a
-      class="my-6 py-2 px-4 border-2 border-slate-600 rounded cursor pointer"
-      :href="card.link"
-      target="_blank"
-      >See on Vercel</a
-    >
-    <div class="flex flex-col lg:flex-row mt-5">
-      <div>
-        <img :src="card.img" alt="img" class="h-[200px] mr-5 mb-5" />
-      </div>
+    <div class="h-full flex flex-col items-center justify-end mt-5">
+      <a
+        class="my-6 py-2 px-4 border-2 border-slate-600 rounded cursor pointer"
+        :href="card.link"
+        target="_blank"
+        >See on Vercel</a
+      >
+
+      <img
+        v-if="card.logo == 'superchord'"
+        src="../assets/superchordexample2.png"
+        alt="img"
+        class="h-[200px] mb-5"
+      />
+      <img
+        v-if="card.logo == 'secretfriend'"
+        src="../assets/secretfriendexample2.png"
+        alt="img"
+        class="h-[200px] mb-5"
+      />
+      <img
+        v-if="card.logo == 'happybirthday'"
+        src="../assets/happybirthdayexample.png"
+        alt="img"
+        class="h-[200px] mb-5"
+      />
+      <img
+        v-if="card.logo == 'pokedex'"
+        src="../assets/pokedexexample.png"
+        alt="img"
+        class="h-[200px] mb-5"
+      />
+      <img
+        v-if="card.logo == 'ecommerce'"
+        src="../assets/ecommerceexample.png"
+        alt="img"
+        class="h-[200px] mb-5"
+      />
     </div>
   </div>
 </template>
--- src/components/js/cards.js
+++ src/components/js/cards.js
@@ -4,19 +4,27 @@ export const cards = [
     vite: true,
     vue: true,
     tailwind: true,
-    logo: '/src/assets/superchord.png',
+    logo: 'superchord',
     description: `System developed to assist the instrumentist's studies or rehearsal;
             In this, it's possible to write chords and, with a simple touch,
             change the tonality, modify chords to include basses by dragging and
             dropping, print PDFs and export or import editable files. The chords
             referring to the tone are ordered and displayed initially, but by
             expanding the control bar, it is possible to add chords that are out
-            the grade.
+            the grade. With limitations, no auth required.
           
          Developed with Vite, Vue.js and TailWind.</p>
           Platform: web.`,
     link: 'https://superchord.vercel.app/',
-    img: '/src/assets/superchordexample2.png',
+  },
+  {
+    title: 'Ecommerce',
+    quasar: true,
+    typescript: true,
+    logo: 'ecommerce',
+    description: `First page of an ecommerce developed with the components of the Quasar Framework. Functionalities: Modal with the description, search products by name, add-to-cart, discount (in this code, generated randomly only to compose the example) and search by category. Using Fake Store Api. No auth required.
+      Platform: web.`,
+    link: 'https://quasar-ecommerce-beta.vercel.app/#/',
   },
   {
     title: 'Secret Friend',
@@ -24,20 +32,18 @@ export const cards = [
     vue: true,
     tailwind: true,
     firebase: true,
-    logo: '/src/assets/secretfriend.png',
+    logo: 'secretfriend',
     description: `Secret Santa's system, developed to raffle names, host in network and
             share the result via hiperlink. Uses Firebase for authentication and
             hosting; After the raffle, a password is generated for each name and
             also generates a text message that can be shared via WhatsApp. In this
             message there is a hiperlink that, with the name and the pass, show
-            "who you raffle."
+            "who you raffle." Auth required.
        
          Developed with Vite, Vue.js, TailWind and Firebase.
          
             Authentication: Email, Facebook or Gmail. Mobile First. Platform: web.`,
     link: 'https://secret-friend-phi.vercel.app/',
-
-    img: '/src/assets/secretfriendexample2.png',
   },
   {
     title: 'Happy Birthday',
@@ -45,42 +51,30 @@ export const cards = [
     vue: true,
     tailwind: true,
     firebase: true,
-    logo: '/src/assets/happybirthday.png',
+    logo: 'happybirthday',
     description: `A mobile app designed to record birthdays; the app orders the list of
             names by date, sends notifications each month reminding the user of
             the birthday list, suggests gifts, tours, trips, etc. It is possible
-            to choose an emoji for every person and the type of relationship.
+            to choose an emoji for every person and the type of relationship. Auth required.
      
          Developed with Ionic Framework, Vue.js, TailWind and Firebase.
        
             Authentication: Email or Gmail. Mobile First. Platform: Mobile
             (Avaliable: Android).`,
     link: 'https://happybirthday-iota.vercel.app/login',
-    img: '/src/assets/happybirthdayexample.png',
   },
   {
     title: 'Pokédex',
     vite: true,
     vue: true,
     tailwind: true,
-    logo: '/src/assets/pokedex.png',
+    logo: 'pokedex',
     description: `A simple Pokédex. To find a pokemon, type its name. 
     The search will return the pokemon, 
-    his stats and his another stages. Using the PokeApi.     
+    his stats and his another stages. Using the PokeApi. No auth required.    
          Developed with Vite, Vue.js and TailWind.       
            Platform: web.
            `,
     link: 'https://pokedex-ebon-eight.vercel.app/',
-    img: '/src/assets/pokedexexample.png',
-  },
-  {
-    title: 'Ecommerce',
-    quasar: true,
-
-    logo: '/src/assets/ecommerce.png',
-    description: `Ecommerce developed with the components of the Quasar Framework. Functionalities: Modal with the description, add-to-cart, discount and search by category. Using Fake Store Api.
-      Platform: web.`,
-    link: 'https://quasar-ecommerce-beta.vercel.app/#/',
-    img: '/src/assets/ecommerceexample.png',
   },
 ]
