150e8c10379d8c819036dadd1deedbacfb01aaa8
DKTDEV04\fabiolocal <fabio.nascimento@fullscreen.com.br>
2024-02-23 17:23:13 -0300

    Redesign tema dark

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,7 @@
-<script setup>
-import Card from './Card.vue'
-import { cards } from './js/cards'
-</script>
+<script setup></script>
 
 <template>
   <div class="flex flex-col justify-center items-center">
-    <div
-      class="opacity-[10%] gif fixed top-0 bottom-0 left-0 w-full h-full"
-    ></div>
     <!-- Header -->
 
     <img
@@ -29,10 +23,92 @@ import { cards } from './js/cards'
       </a>
     </div>
 
-    <h2 class="font-bold invert mb-6">:: Projects and Technologies ::</h2>
-    <!-- Superchord -->
-    <section class="flex flex-wrap justify-center">
-      <Card v-for="(item, index) in cards" :key="index" :card="item" />
+    <h3 data-aos="fade-right" class="mt-8 text-[30px] lg:text-[40px]">
+      { Projetos e Tecnologias }
+    </h3>
+
+    <section class="flex flex-col justify-center items-center">
+      <h1 data-aos="fade-right" class="mt-[50px]">IBV APP</h1>
+      <div class="iconArea" data-aos="fade-right">
+        <img src="../assets/reactnative.png" alt="" class="icon" />
+        <img src="../assets/firebase.png" alt="" class="icon" />
+        <img src="../assets/styledcomponents.png" alt="" class="icon" />
+      </div>
+      <h3 data-aos="fade-right" class="text-center">
+        Desenvolvido em React Native, o conteúdo do aplicativo é alimentado por
+        um banco de dados do firebase. A gestão é feita em um painal
+        administrativo à parte. Estilizado com Styled Components.
+      </h3>
+      <img
+        data-aos="fade-right"
+        src="../assets/ibvapp.png"
+        alt=""
+        class="w-[100vw] lg:w-[80vw]"
+      />
+      <h1 data-aos="fade-right" class="mt-[50px]">IBV ADMIN</h1>
+      <div class="iconArea" data-aos="fade-right">
+        <img src="../assets/reactnative.png" alt="" class="icon" />
+        <img src="../assets/firebase.png" alt="" class="icon" />
+        <img src="../assets/tailwind.png" alt="" class="icon" />
+      </div>
+      <h3 data-aos="fade-right" class="text-center">
+        Ambiente de gestão do aplicativo IBV APP, desenvolvido em React. Acesso
+        via autenticação; banco de dados: Firebase. Estilizado com TailwindCSS.
+      </h3>
+      <img
+        data-aos="fade-right"
+        src="../assets/ibvadmin.png"
+        alt=""
+        class="w-[100vw] lg:w-[80vw]"
+      />
+
+      <h1 data-aos="fade-right" class="mt-[50px]">SUPERCHORD</h1>
+      <div class="iconArea" data-aos="fade-right">
+        <img src="../assets/vite.png" alt="" class="icon" />
+        <img src="../assets/vue2.png" alt="" class="icon" />
+        <img src="../assets/tailwind.png" alt="" class="icon" />
+      </div>
+      <h3 data-aos="fade-right" class="text-center">
+        Sistema desenvolvido para auxiliar o estudo musical e a criação/edição
+        de cifras. Tecnologias utilizadas: VueJs, Firebase e TailwindCSS.
+      </h3>
+      <a
+        class="darkbutton"
+        data-aos="fade-right"
+        href="https://superchord.vercel.app"
+        target="_blank"
+        >ACESSAR</a
+      >
+      <img
+        data-aos="fade-right"
+        src="../assets/superchordmookup.png"
+        alt=""
+        class="w-[100vw] lg:w-[80vw]"
+      />
+
+      <h1 data-aos="fade-right" class="mt-[50px]">CARD YOURSELF</h1>
+      <div class="iconArea" data-aos="fade-right">
+        <img src="../assets/vite.png" alt="" class="icon" />
+        <img src="../assets/vue2.png" alt="" class="icon" />
+        <img src="../assets/ionic.png" alt="" class="icon" />
+      </div>
+      <h3 data-aos="fade-right" class="text-center">
+        Jogo estilo card-game multiplataforma (Web e Mobile).
+      </h3>
+      <a
+        class="darkbutton"
+        data-aos="fade-right"
+        href="https://card-yourself.vercel.app"
+        target="_blank"
+        >ACESSAR</a
+      >
+      <img
+        data-aos="fade-right"
+        src="../assets/cardyourself.png"
+        alt=""
+        class="w-[100vw] lg:w-[80vw]"
+      />
+      <!-- <Card v-for="(item, index) in cards" :key="index" :card="item" /> -->
     </section>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css' // You can also use <link> for styles
+// ..
+AOS.init({
+  duration: 600,
+  startEvent: 'DOMContentLoaded',
+  easing: 'ease-in-out-cubic',
+  once: true,
+  useClassNames: true,
+  offset: 120,
+})
 
 createApp(App).mount('#app')
