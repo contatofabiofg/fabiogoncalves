6997fe2b6fa87ffdd30bd7a4c7b649b6cc43536e
DKTDEV04\fabiolocal <fabio.nascimento@fullscreen.com.br>
2024-07-11 14:53:49 -0300

    ajuste no hover dos botões de whatsapp e email

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,24 +11,37 @@
     />
 
     <div class="flex mb-[20px] lg:mb-[30px] invert">
-      <a href="https://wa.me/5522981135860" target="_blank">
+      <a
+        href="https://wa.me/5522981135860"
+        target="_blank"
+        class="hover:bg-transparent"
+      >
         <img
           src="../assets/whatsapp.png"
           alt="Whatsapp button"
           class="w-6 mr-3"
         />
       </a>
-      <a href="mailto:contatofabiofg@hotmail.com.br" target="_blank">
+      <a
+        href="mailto:contatofabiofg@hotmail.com.br"
+        target="_blank"
+        class="hover:bg-transparent"
+      >
         <img src="../assets/email.png" alt="Email Button" class="w-6" />
       </a>
     </div>
-<div data-aos="fade-right"  class="border border-slate-300 p-[15px] rounded-lg max-w-[95vw]">
-    <h3 class="text-center text-sm">
-Abaixo estão os meus projetos pessoais. Já atuei em outros projetos de cunho empresarial (ERP), que envolvem a manipulação de mapas (sistema de entregas) e no desenvolvimento de jogos educativos. Por se tratarem de projetos de terceiros, não estão nessa lista.      </h3>
-
-    
-  </div>
-  <h3 data-aos="fade-right" class="mt-8 text-[30px] lg:text-[40px]">
+    <div
+      data-aos="fade-right"
+      class="border border-slate-300 p-[15px] rounded-lg max-w-[95vw]"
+    >
+      <h3 class="text-center text-sm">
+        Abaixo estão os meus projetos pessoais. Já atuei em outros projetos de
+        cunho empresarial (ERP), que envolvem a manipulação de mapas (sistema de
+        entregas) e no desenvolvimento de jogos educativos. Por se tratarem de
+        projetos de terceiros, não estão nessa lista.
+      </h3>
+    </div>
+    <h3 data-aos="fade-right" class="mt-8 text-[30px] lg:text-[40px]">
       { Projetos e Tecnologias }
     </h3>
 
@@ -40,7 +53,10 @@ Abaixo estão os meus projetos pessoais. Já atuei em outros projetos de cunho e
         <img src="../assets/node.png" alt="" class="icon" />
       </div>
       <h3 data-aos="fade-right" class="text-center">
-        Sistema de triagem de pacientes em uma unidade hospitalar, com frontend desenvolvido em Next.js/Tailwind e backend desenvolvido em Node.js. A comunicação é feita através de Socket.io. As etapas são: acolhimento, classificação de risco e consultório. As chamadas são exibidas na tela. 
+        Sistema de triagem de pacientes em uma unidade hospitalar, com frontend
+        desenvolvido em Next.js/Tailwind e backend desenvolvido em Node.js. A
+        comunicação é feita através de Socket.io. As etapas são: acolhimento,
+        classificação de risco e consultório. As chamadas são exibidas na tela.
       </h3>
       <a
         class="darkbutton"
@@ -97,7 +113,10 @@ Abaixo estão os meus projetos pessoais. Já atuei em outros projetos de cunho e
       </div>
       <h3 data-aos="fade-right" class="text-center">
         Sistema desenvolvido para auxiliar o estudo musical e a criação/edição
-        de cifras. As duas grandes sacadas são: é possível escrever uma cifra em uma tonalidade e, na sequência, alterar a tonalidade - adpatandos os acordes; há um modo de impressão de várias cifras em uma única folha. Tecnologias utilizadas: VueJs, Firebase e TailwindCSS.
+        de cifras. As duas grandes sacadas são: é possível escrever uma cifra em
+        uma tonalidade e, na sequência, alterar a tonalidade - adpatandos os
+        acordes; há um modo de impressão de várias cifras em uma única folha.
+        Tecnologias utilizadas: VueJs, Firebase e TailwindCSS.
       </h3>
       <a
         class="darkbutton"
@@ -120,7 +139,10 @@ Abaixo estão os meus projetos pessoais. Já atuei em outros projetos de cunho e
         <img src="../assets/ionic.png" alt="" class="icon" />
       </div>
       <h3 data-aos="fade-right" class="text-center">
-        Jogo estilo card-game multiplataforma (Web e Mobile). As imagens foram geradas utilizando inteligência artificial; o jogo foi desenvolvido em Vue/Ionic. Nesta versão para exibição, o jogador já inicia com pontos e chaves para distribuir e acompanhar a evolução dos monstrinhos.
+        Jogo estilo card-game multiplataforma (Web e Mobile). As imagens foram
+        geradas utilizando inteligência artificial; o jogo foi desenvolvido em
+        Vue/Ionic. Nesta versão para exibição, o jogador já inicia com pontos e
+        chaves para distribuir e acompanhar a evolução dos monstrinhos.
       </h3>
       <a
         class="darkbutton"
